f121979c0d9ac950c4006ebe2390c39f1641f2c5
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2019-11-09 20:25:07 +0200

    update App component with a select of countries from a gql api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// initialize a GraphQL client
+const client = new ApolloClient({
+    uri: 'https://countries.trevorblades.com',
+});
+
+// write a GraphQL query that asks for names and codes for all countries
+const GET_COUNTRIES = gql`
+    {
+        countries {
+            name
+            code
+        }
+    }
+`;
+
+const App = () => {
+    const [country, setCountry] = useState('Andorra');
+
+    return (
+        <Router basename='/countries'>
+            <h1>{country}</h1>
+            <Query query={GET_COUNTRIES} client={client}>
+                {({ loading, error, data }) => {
+                    if (loading) return <p>Loading...</p>;
+                    if (error) return <p>{error.message}</p>;
+                    return (
+                        <div>
+                            <select value={country} onChange={(e) => setCountry(e.target.value)}>
+                                {data.countries.map((country) => (
+                                    <option key={country.name} value={country.name}>
+                                        {country.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    );
+                }}
+            </Query>
+
+            <Link to='hello'>Hello</Link>
+        </Router>
+    );
+};
 
 export default App;
