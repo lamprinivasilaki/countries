6998fac83cb03a0b4341b855c096203005804420
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2019-11-18 16:31:37 +0200

    add missing types

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -7,21 +7,27 @@ import {
     Typography,
 } from '@material-ui/core';
 import RootRef from '@material-ui/core/RootRef';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+    DragDropContext,
+    Droppable,
+    Draggable,
+    DropResult,
+    OnDragEndResponder,
+    DraggableStyle,
+    DraggingStyle,
+} from 'react-beautiful-dnd';
 import { CountryEntity } from '../interfaces/country.interface';
 
 interface PropsType {
     items: CountryEntity[];
 }
-const getListStyle = isDraggingOver => ({
+const getListStyle = (isDraggingOver: boolean): DraggingStyle => ({
     width: 400,
     background: isDraggingOver ? 'lightblue' : '#efefef',
 });
 
-const getItemStyle = (isDragging, draggableStyle) => ({
-    // styles we need to apply on draggables
-    ...draggableStyle,
-
+const getItemStyle = (isDragging: boolean, style: DraggableStyle): DraggableStyle => ({
+    ...style,
     ...(isDragging && {
         background: 'rgb(235,235,235)',
     }),
@@ -42,13 +48,13 @@ const Column: FunctionComponent<PropsType> = ({ items }) => {
         return updatedList;
     };
 
-    const onDragEnd = event => {
+    const onDragEnd = (result: DropResult): OnDragEndResponder => {
         // dropped outside the list
-        if (!event.destination) {
+        if (!result.destination) {
             return;
         }
 
-        setList(reorder(list, event.source.index, event.destination.index));
+        setList(reorder(list, result.source.index, result.destination.index));
     };
 
     return (
