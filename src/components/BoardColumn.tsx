2eacaae63ccb1ca413acaa01d392d1324ba9030d
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2019-11-25 20:59:11 +0200

    add board columns background conditionally

--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -4,6 +4,8 @@ import { CountryEntity } from '../interfaces/country.interface';
 import BoardItem from './BoardItem';
 import styled from 'styled-components';
 import { ColumnEntity } from '../interfaces/column.interface';
+import africa from '../assets/africa.png';
+import asia from '../assets/asia.png';
 
 interface Props {
     key?: string;
@@ -14,6 +16,7 @@ interface Props {
 
 interface StyleProps {
     isDraggingOver: boolean;
+    continentCode: string;
 }
 
 const BoardColumnWrapper = styled.div`
@@ -31,11 +34,22 @@ const BoardColumnTitle = styled.h2`
     font: 14px sans-serif;
     margin-bottom: 12px;
 `;
-
+// background: ${props => {
+//     console.log('props: ', props);
+//     return `url(../assets/${props.continentCode}.png) no-repeat top center`;
+// }};
 const BoardColumnContent = styled.div<StyleProps>`
-    min-height: 20px;
+    min-height: 50px;
     background-color: ${props => (props.isDraggingOver ? '#aecde0' : null)};
     border-radius: 4px;
+    background: ${props =>
+        `url(${
+            props.continentCode === 'AF'
+                ? africa
+                : props.continentCode === 'AS'
+                ? asia
+                : null
+        }) no-repeat top center`}};
 `;
 const BoardColumn: FunctionComponent<Props> = ({
     items,
@@ -53,6 +67,7 @@ const BoardColumn: FunctionComponent<Props> = ({
                             {...provided.droppableProps}
                             ref={provided.innerRef}
                             isDraggingOver={snapshot.isDraggingOver}
+                            continentCode={column.id}
                         >
                             {items.map((item: CountryEntity, index: number) => (
                                 <BoardItem
