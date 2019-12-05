32a35e8cc54cd097d3af44523440ede3f8c29416
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2019-12-05 20:43:48 +0200

    display <Details /> insteaf of <Card />

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,32 +1,18 @@
 import React, { FunctionComponent } from 'react';
 import CountriesQuery from '../api/queries/countries';
 import { useQuery } from '@apollo/react-hooks';
-import {
-    CircularProgress,
-    Card,
-    CardContent,
-    Typography,
-} from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 import Alert from './Alert';
 import { CountryEntity } from '../interfaces/country.interface';
 import { makeStyles } from '@material-ui/styles';
 import CountriesTable from './CountriesTable';
+import Details from './Details';
 
 const useStyles = makeStyles({
     container: {
         display: 'flex',
         flexWrap: 'wrap',
         justifyContent: 'space-between',
-    },
-    card: {
-        margin: 10,
-        flexGrow: 1,
-    },
-    content: {
-        padding: '30px !important',
-    },
-    cardTitle: {
-        marginBottom: 15,
     },
 });
 
@@ -47,24 +33,15 @@ const CountriesList: FunctionComponent = () => {
             <div className={classes.container}>
                 {data && (
                     <CountriesTable countries={data.countries}></CountriesTable>
-                )}
-                {/*{data &&
+                )} */}
+                {data &&
                     data.countries.map((country: CountryEntity) => (
-                        <Card className={classes.card} key={country.code}>
-                            <CardContent className={classes.content}>
-                                <Typography
-                                    variant="h5"
-                                    className={classes.cardTitle}
-                                >
-                                    {country.emoji} {country.name}
-                                </Typography>
-                                <Typography color="textSecondary">
-                                    <b>{country.name}</b> is a country located
-                                    in <b>{country.continent.name}</b>.
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))} */}
+                        <Details
+                            country={country}
+                            showDetails={true}
+                            key={country.name}
+                        ></Details>
+                    ))}
             </div>
         </>
     );
