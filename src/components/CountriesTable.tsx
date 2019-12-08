d080ea1f96b32d6db5720a6e04773fd52048d3e4
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2019-12-08 18:21:40 +0200

    add pagination in countries table component

--- src/components/CountriesTable.tsx
+++ src/components/CountriesTable.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import {
     Paper,
     Table,
@@ -6,6 +6,7 @@ import {
     TableRow,
     TableCell,
     TableBody,
+    TablePagination,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { CountryEntity } from '../interfaces/country.interface';
@@ -26,6 +27,19 @@ const useStyles = makeStyles({
 
 const CountriesTable: FunctionComponent<Props> = ({ countries }) => {
     const classes = useStyles();
+    const [page, setPage] = useState(0);
+    const [countriesPerPage, setCountriesPerPage] = useState(5);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        setCountriesPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     return (
         <Paper className={classes.root}>
@@ -40,31 +54,45 @@ const CountriesTable: FunctionComponent<Props> = ({ countries }) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {countries.map((country: CountryEntity) => (
-                        <TableRow key={country.name}>
-                            <TableCell component="th" scope="row">
-                                {country.emoji}
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                {country.name}
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                {country.continent.name}
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                {country.currency}
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                {country.languages.length !== 0
-                                    ? country.languages
-                                          .map(language => language.name)
-                                          .join()
-                                    : null}
-                            </TableCell>
-                        </TableRow>
-                    ))}
+                    {countries
+                        .slice(
+                            page * countriesPerPage,
+                            page * countriesPerPage + countriesPerPage,
+                        )
+                        .map((country: CountryEntity) => (
+                            <TableRow key={country.name}>
+                                <TableCell component="th" scope="row">
+                                    {country.emoji}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {country.name}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {country.continent.name}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {country.currency}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {country.languages.length !== 0
+                                        ? country.languages
+                                              .map(language => language.name)
+                                              .join()
+                                        : null}
+                                </TableCell>
+                            </TableRow>
+                        ))}
                 </TableBody>
             </Table>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={countries.length}
+                rowsPerPage={countriesPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
         </Paper>
     );
 };
