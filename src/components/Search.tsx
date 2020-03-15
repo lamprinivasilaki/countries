5eba403a5eb91e4f89e18dfa870a90573ae95087
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2020-03-15 15:58:58 +0200

    remove forgotten unused imports

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Autocomplete } from '@material-ui/lab';
 import { TextField } from '@material-ui/core';
-import { CountryEntity } from '../interfaces/country.interface';
 
 interface Props {
     value: string | undefined;
